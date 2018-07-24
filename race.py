9d549e721c3ae5979aec89312c0d049b840f1f1b
Michael B <michael@ramyun.co.uk>
2018-07-24 20:56:51 +0100

    Cleaned up race.py

--- race.py
+++ race.py
@@ -1,21 +1,31 @@
+import sys
 import selenium.webdriver as webdriver
 import selenium.webdriver.support.ui as ui
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from bs4 import BeautifulSoup
 
 typeracer_url = 'https://play.typeracer.com/'
+element_load_timeout = 45 # (seconds)
+
+# Load game url from command arguments, if any
+race_url = sys.argv[1] if len(sys.argv) > 1 else typeracer_url
+multiplayer_race = race_url != typeracer_url
+
+# Initialise chromedriver stuff
 driver = webdriver.Chrome()
+wait = ui.WebDriverWait(driver, element_load_timeout)
 
-try:
+# Launches and starts a race
+def launch_race(multiplayer=True):
     # Boot chromedriver and wait for page to load
-    driver.get(typeracer_url)
-    wait = ui.WebDriverWait(driver, 10)
-    wait.until(lambda driver: driver.find_element_by_link_text('Practice'))
+    driver.get(race_url)
 
-    # Locate and press practice button
-    practice_btn = driver.find_element_by_link_text('Practice')
-    practice_btn.click()
+    # Press start button
+    start_btn = wait.until(lambda driver: driver.find_element_by_partial_link_text('join race') if multiplayer else driver.find_element_by_link_text('Practice'))
+    start_btn.click()
 
+# Bosses an ongoing race
+def win_race():
     # Wait for race to load & start
     wait.until(lambda driver: driver.find_element_by_xpath("//*[contains(text(), 'The race is on! Type the text below:')]"))
     input_field = driver.find_element_by_class_name('txtInput')
@@ -29,15 +39,20 @@ try:
     for span in soup.body.find_all('span', attrs={'unselectable': 'on'}):
         race_text += span.getText()
 
-    print race_text
+    print "Race text: " + race_text
 
     # Enter race text into input field
     input_field.click()
     input_field.send_keys(race_text)
 
-    # Win
+try:
+    launch_race(multiplayer_race)
+    win_race()
 
 except TimeoutException:
     print "Error: An element could not be located within timeout duration"
+    driver.quit()
 
-# finally: driver.quit()
+except WebDriverException:
+    print "Error: Invalid TypeRacer url"
+    driver.quit()
