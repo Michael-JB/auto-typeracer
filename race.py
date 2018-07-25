3e06a0bb7cdcb26034f08e5d34e3b59cd1b417d9
Michael B <michael@ramyun.co.uk>
2018-07-25 23:20:26 +0100

    Added optional command line arguments

--- race.py
+++ race.py
@@ -1,24 +1,31 @@
 from __future__ import division
 import sys
 import time
+import argparse
 import selenium.webdriver as webdriver
 import selenium.webdriver.support.ui as ui
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from bs4 import BeautifulSoup
 
 typeracer_url = 'https://play.typeracer.com/'
-element_load_timeout = 45 # (seconds)
+element_load_timeout = 360 # (seconds)
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="set the url of a multiplayer game. By default, a practice game is launched")
+parser.add_argument("--wpm", help="set target wpm, by default maximum speed is used", type=int)
+args = parser.parse_args()
 
 # Set and calculate typing pace
-target_wpm = 100
+max_speed_threshold = 1000 # (wpm)
+target_wpm = args.wpm if args.wpm else max_speed_threshold
+type_at_max_speed = target_wpm >= max_speed_threshold
 avg_word_length = 5.1
 target_spc = 1 / ((target_wpm / 60) * avg_word_length)
 
-type_at_max_speed = True
-
 # Load game url from command arguments, if any
-race_url = sys.argv[1] if len(sys.argv) > 1 else typeracer_url
-multiplayer_race = race_url != typeracer_url
+multiplayer_race = args.url
+race_url = args.url if multiplayer_race else typeracer_url
 
 # Initialise chromedriver stuff
 driver = webdriver.Chrome()
@@ -29,7 +36,7 @@ def launch_race(multiplayer=True):
     # Boot chromedriver and wait for page to load
     driver.get(race_url)
 
-    # Press start button
+    # Wait for and subsequently press start button
     start_btn = wait.until(lambda driver: driver.find_element_by_partial_link_text('join race') if multiplayer else driver.find_element_by_link_text('Practice'))
     start_btn.click()
 
@@ -44,7 +51,7 @@ def win_race():
     soup = BeautifulSoup(html, 'lxml')
 
     # Scrape race text from soup
-    race_text = "";
+    race_text = ""
     for span in soup.body.find_all('span', attrs={'unselectable': 'on'}):
         race_text += span.getText()
 
